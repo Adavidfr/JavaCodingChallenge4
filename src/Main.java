ef34533e2779e500f7b9d87ceea2a6007e09d42f
Alex David Frías <adavidfruiz@gmail.com>
2024-06-19 00:38:51 -0500

    feat: add main method for bank account operations

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+
+        Bank bank = new Bank();
+
+        bank.addAccount("12345", "David", 2234.45);
+        bank.addAccount("12234", "Katy", 5697.09);
+        bank.addAccount("23445", "Maya", 12.35);
+
+        System.out.println("===========Create accounts==========");
+        for (BankAccount account : bank.listAllAccounts()) {
+            System.out.println(account);
+        }
+
+        System.out.println("===========Find account by number==========");
+        System.out.println(bank.findAccount("12234"));
+
+        System.out.println("===========Remove account by number==========");
+        bank.removeAccount("23445");
+        for (BankAccount account : bank.listAllAccounts()) {
+            System.out.println(account);
+        }
+
+        System.out.println("===========Transfer to another account==========");
+        bank.transfer("12345", "12234", 245.90);
+        for (BankAccount account : bank.listAllAccounts()) {
+            System.out.println(account);
+        }
+
     }
 
 }
